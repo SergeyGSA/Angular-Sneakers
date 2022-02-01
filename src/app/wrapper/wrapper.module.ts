ec44c3260988d2ad064613606298c02d63a8fc2e
SergeyGSA <goreslavskiy.sergey@gmail.com>
2022-02-01 19:34:25 +0200

    add search

--- src/app/wrapper/wrapper.module.ts
+++ src/app/wrapper/wrapper.module.ts
@@ -5,8 +5,13 @@ import { CommonModule } from "@angular/common";
 import { WrapperComponent } from './components/wrapper/wrapper.component';
 import { HeaderComponent } from '../wrapper/components/header/header.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
+import { SearchComponent } from './components/search/search.component';
 
 import { NgbModule, NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
 
 const routes: Routes = [
   {
@@ -19,13 +24,17 @@ const routes: Routes = [
   declarations: [
     WrapperComponent,
     HeaderComponent,
-    CarouselComponent
+    CarouselComponent,
+    SearchComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
     NgbModule,
     NgbCarouselModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
   ],
   providers: []
 })
